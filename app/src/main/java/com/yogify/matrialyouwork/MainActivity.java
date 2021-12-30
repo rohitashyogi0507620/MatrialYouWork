1e5e9f5d8a0678a5bb25303066516a7e8ef0fdbd
RohitashYogi <rohitash.yogi@probusinsurance.com>
2021-12-30 13:32:16 +0530

    New Change

--- app/src/main/java/com/yogify/matrialyouwork/MainActivity.java
+++ app/src/main/java/com/yogify/matrialyouwork/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(), "Trying New Listing ", Toast.LENGTH_SHORT).show();
         Toast.makeText(getApplicationContext(), "You Are New Bro", Toast.LENGTH_SHORT).show();
 
 
